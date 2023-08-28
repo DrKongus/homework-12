1244bfc81e5a634378d2323571bc7d8617e2508c
Fedor <f.tkachov@gmail.com>
2023-08-28 12:38:13 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,45 @@
+// -----1---- //
+// Напишіть функцію printNumbers(from, to) яка виводить число кожну секунду,
+// починаючи від from і закінчуючи to.
+// Зробіть два варіанти рішення.
+// Використовуючи setInterval.
+// Використовуючи вкладений setTimeout
+
+// -- Відповідь -- //
+function printNumbers(from, to) {
+  let num = from;
+
+  //   setInterval //
+  let timerId = setInterval(function () {
+    console.log(num);
+    if (num == to) {
+      clearInterval(timerId);
+    }
+
+    // setTimeout //
+    //   setTimeout(function go() {
+    //     console.log(num);
+    //     if (num < to) {
+    //       setTimeout(go, 1000);
+    //     }
+
+    //
+    num++;
+  }, 1000);
+}
+printNumbers(0, 10);
+
+// -----2---- //
+// Вбудована функція setTimeout використовує колбек-функції. Створіть
+// альтернативу яка базується на промісах.
+// Функція delay(ms) повинна повертати проміс, який перейде в стан resolved через
+// ms мілісекунд, так щоб ми могли додати до нього .then:
+// function delay(ms) {
+// // ваш код
+// }delay(3000).then(() => alert('виконалось через 3 секунди'));
+
+// -- Відповідь -- //
+// function delay(ms) {
+//   return new Promise((resolve) => setTimeout(resolve, ms));
+// }
+// delay(3000).then(() => alert("виконалось через 3 секунди"));
